2daeec77b649bd9605e7ae6daf7aa1a72a1d0960
Shulepa Volodymyr <shulepatrader@gmail.com>
2023-01-31 17:28:04 +0200

    fix something

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,17 +15,6 @@ const styles = {
 
 export const Footer = () => (
   <footer style={styles.footer}>
-    <p>PhoneBook &copy; 2023 by&nbsp;</p>{' '}
-    <a
-      style={styles.link}
-      href="https://github.com/KsushkaKov"
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      <span role="img" aria-label="Telephone icon">
-        ☎️{' '}
-      </span>{' '}
-      Ksenia Kovalenko
-    </a>
+    <p>PhoneBook &copy; 2023</p>{' '}
   </footer>
 );
